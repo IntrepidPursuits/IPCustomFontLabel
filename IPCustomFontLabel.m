ebfdfc9ce9de588a1fcb8fcdc419f2652631b8e8
Matt Bridges <matt@intrepid.io>
2013-05-08 14:20:07 -0400

    Initial commit.

---
+++ IPCustomFontLabel.m
@@ -0,0 +1,31 @@
+#import "IPCustomFontLabel.h"
+
+@interface IPCustomFontLabel ()
+@property (copy, nonatomic) NSString *fontFamily;
+@end
+
+@implementation IPCustomFontLabel
+
+- (id)initWithFrame:(CGRect)frame font:(UIFont *)font
+{
+    self = [self initWithFrame:frame];
+    if (self) {
+        self.font = font;
+    }
+    return self;
+}
+
+- (void)awakeFromNib
+{
+    if (self.fontFamily && self.font) {
+        CGFloat fontSize = self.font.pointSize;
+        UIFont *font = [UIFont fontWithName:self.fontFamily size:fontSize];
+        if (font) {
+            self.font = font;
+        } else {
+            NSLog(@"Warning: Could not instantiate UIFont with family %@ for %@", self.fontFamily, [self description]);
+        }
+    }
+}
+
+@end
